9b736ab24ee189c45d25d57de009b0fb6111e59b
ecifuentesdv <eacdv15@gmail.com>
2025-09-28 15:05:06 -0600

    creacion del repositorio

--- index.js
+++ index.js
@@ -10,17 +10,6 @@ app.use(express.json());
 app.use('/clientes', userRoutes);
 app.use('/ordenes', taskRoutes);
 
-// Configuración para servir frontend
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
-
-app.use(express.static(path.join(__dirname, 'public')));
-
-// Para rutas SPA (cualquier otra ruta devuelve index.html)
-app.get(/^\/.*$/, (req, res) => {
-  res.sendFile(path.join(__dirname, 'public', 'index.html'));
-});
-
 
 const PORT = process.env.PORT || 3030;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
